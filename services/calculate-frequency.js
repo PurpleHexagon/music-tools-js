a8648d7c5d760a6304a1214a47dce0936f9fe2cc
Jon Day <jon@plantigrate.com>
2020-01-13 13:03:21 +0000

    Move math to use mathjs

--- services/calculate-frequency.js
+++ services/calculate-frequency.js
@@ -1,12 +1,33 @@
+import { add, bignumber, divide, format, multiply, pow, subtract } from 'mathjs'
 /**
  * Calculate frequency for interval and octave
+ *
+ * referenceFrequency * 2 ** ((octave - 4) + (interval / 12))
  *
  * @param referenceFrequency
  * @param interval
  * @param octave
  * @returns {number}
  */
-const calculateFrequency = (referenceFrequency, interval, octave) =>
-    (referenceFrequency * 2 ** ((octave - 4) + (interval / 12)))
+const calculateFrequency = (referenceFrequency, interval, octave) => (
+    format(
+        multiply(
+            referenceFrequency,
+            pow(
+                bignumber('2'),
+                add(
+                    subtract(
+                        octave, bignumber('4')
+                    ),
+                    divide(
+                        interval,
+                        bignumber('12')
+                    )
+                )
+            )
+        ),
+        { notation: 'fixed', precision: 16 }
+    ).toString()
+)
 
 export default calculateFrequency
